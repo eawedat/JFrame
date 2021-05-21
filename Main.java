1b9de61fa1f2387435cda6fa5c9cd3bd8d763320
Muhamad Eawedat <31136806+eawedat@users.noreply.github.com>
2021-05-21 22:31:17 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,29 @@
+package com.eawedat;
+
+import oracle.jrockit.jfr.JFR;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       /* JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //frame.setResizable(false);
+        frame.setTitle("Hello");
+        frame.setSize(420,420);
+        frame.setVisible(true);
+
+        ImageIcon imageIcon = new ImageIcon("lion.png");
+        frame.setIconImage(imageIcon.getImage());
+
+        frame.getContentPane().setBackground(Color.GREEN);
+        //frame.getContentPane().setBackground(new Color(0,255,0));
+        //frame.getContentPane().setBackground(new Color(0xFFFFFF));*/
+
+       //MyFrame myFrame = new MyFrame();
+       new MyFrame();
+    }
+}
