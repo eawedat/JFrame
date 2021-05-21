c5590098b98e360a930d8e692dd5b8770e7a363c
Muhamad Eawedat <31136806+eawedat@users.noreply.github.com>
2021-05-21 22:35:41 +0300

    Create JLabel_text.java

---
+++ JLabel_text.java
@@ -0,0 +1,35 @@
+package com.eawedat;
+
+import oracle.jrockit.jfr.JFR;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //frame.setResizable(false);
+        frame.setTitle("Hello");
+        frame.setSize(500,500);
+        frame.setVisible(true);
+
+        ImageIcon imageIcon = new ImageIcon("lion.png");
+        frame.setIconImage(imageIcon.getImage());
+
+
+        JLabel label = new JLabel();
+        label.setText("Muhamad eawedat");
+
+
+        frame.add(label);
+        //frame.getContentPane().setBackground(Color.GREEN);
+        //frame.getContentPane().setBackground(new Color(0,255,0));
+        //frame.getContentPane().setBackground(new Color(0xFFFFFF));
+
+       //MyFrame myFrame = new MyFrame();
+       //new MyFrame();
+    }
+}
